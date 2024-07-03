888a8f2d3a139717de77af664ea827f5397cd229
Sourov Kumar <sourovsarker-21ia@dipti.com.bd>
2024-07-04 00:28:09 +0600

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed-text", {
+    strings: ["Computer Science Student", "Web Developer", "Frontend Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
